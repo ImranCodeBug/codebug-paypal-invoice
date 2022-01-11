f3b4c622317559a4f4e6dd2d36050381b8901ded
Imran <imran.loves.to.code@gmail.com>
2022-01-11 23:47:25 +0600

    initial commit

---
+++ PaypalInvoice/Components/MainComponent.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+
+interface Props {
+    context : any;
+}
+
+export const MainComponent = (props: Props) => {
+    //console.log(props.context);
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
